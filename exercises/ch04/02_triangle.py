3d70b537b9d0edad4059343687176f69efae15f6
rivasalvaroy <rivasalvaroy@gmail.com>
2023-03-06 13:00:17 -0600

    ch04_02 - triangle

---
+++ exercises/ch04/02_triangle.py
@@ -0,0 +1,42 @@
+import turtle
+
+t = turtle.Turtle()
+# An equilateral triangle
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.reset()
+
+# An isosceles triangle
+t.forward(50)
+t.left(104.47751218592992)
+t.forward(100)
+t.left(151.04497562814015)
+t.forward(100)
+t.left(104.47751218592992)
+t.reset()
+
+# Right scalene triangle
+t.forward(100)
+t.left(149)
+t.forward(117)
+t.left(119)
+t.forward(60)
+t.reset()
+
+# Obtuse scalene triangle
+t.forward(100)
+t.left(153.9)
+t.forward(131)
+t.left(133.1)
+t.forward(60)
+t.reset()
+
+# Acute scalene triangle
+t.forward(100)
+t.left(144)
+t.forward(93)
+t.left(101.2)
+t.forward(60)
