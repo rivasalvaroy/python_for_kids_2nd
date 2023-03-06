44704325fd0f6f7afbf4184342e227a8c7520ddc
rivasalvaroy <rivasalvaroy@gmail.com>
2023-03-06 08:38:45 -0600

    ch03_05 - list of list

---
+++ examples/ch03/05_list_of_lists.py
@@ -0,0 +1,12 @@
+some_numbers = [1, 2, 5, 10, 20]
+some_strings = ['Which', 'Witch', 'Is', 'Which']
+
+numbers_and_strings = ['Why', 'was', 6, 'afraid', 'of', 7, 'because', 7, 8, 9]
+print(numbers_and_strings)
+
+numbers = [1, 2, 3, 4]
+strings = ['I', 'kicked', 'my', 'toe', 'and', 'it', 'is', 'sore']
+mylist = [numbers, strings]
+print(mylist)
+print(mylist[0])
+print(mylist[1])
