661facfc1ce4e82ce43958f91e5261b1fe73333c
rivasalvaroy <rivasalvaroy@gmail.com>
2023-03-06 17:20:06 -0600

    ch05_01 - if statements

---
+++ examples/ch05/01_if_statements.py
@@ -0,0 +1,24 @@
+age = 25
+if age > 20:
+    print('You are too old!')
+    print('Why are you here?')
+    print('Why aren\'t you mowing a lawn or sorting papers?')
+
+age = 10
+if age > 10:
+    print('You are too old for my jokes!')
+
+age = 10
+if age >= 10:
+    print('You are too old for my jokes!')
+
+age = 10
+if age == 10:
+    print("What's brown and sticky? A stick!! ")
+
+print("Want to hear a dirty joke?")
+age = 12
+if age == 12:
+    print("A pig fell in the mud!")
+else:
+    print("Shh. It's a secret.")
