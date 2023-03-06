3016101f5a0983f9fe9ee3a9878880e8a93264b9
rivasalvaroy <rivasalvaroy@gmail.com>
2023-03-06 12:16:27 -0600

    ch03_04 - multiline letter

---
+++ exercises/ch03/04_multiline_letter.py
@@ -0,0 +1,12 @@
+spaces = ' ' * 25
+print(f'''{spaces}12 Butts Wynd
+{spaces}Twinklebottom Heath
+{spaces}West Snoring
+
+Dear Sir
+
+I wish to report that tiles are missing from the outside toilet roof.
+I think it was bad wind the other night that blew them away.
+
+Regards
+Malcolm Dithering''')
