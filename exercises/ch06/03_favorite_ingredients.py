0b500cbc75e8336feee0888f4d0cd349484359cc
rivasalvaroy <rivasalvaroy@gmail.com>
2023-03-07 21:34:15 -0600

    ch06_03 - favorite ingredients

---
+++ exercises/ch06/03_favorite_ingredients.py
@@ -0,0 +1,7 @@
+ingredients = ['snails', 'leeches', 'gorilla belly-button lint',
+               'caterpillar eyebrows', 'centipede toes']
+
+x = 1
+for i in ingredients:
+    print(f'{x} {i}')
+    x += 1
