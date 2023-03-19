24cc3d9ea7430f6e518f193703c7a6320f9f0b49
rivasalvaroy <rivasalvaroy@gmail.com>
2023-03-19 14:47:29 -0600

    ch08_06 - four small spirals v2

--- exercises/ch08/04_four_small_spirals.py
+++ exercises/ch08/04_four_small_spirals.py
@@ -1,60 +1,29 @@
 import turtle
 
-t1 = turtle.Pen()
-t2 = turtle.Pen()
-t3 = turtle.Pen()
-t4 = turtle.Pen()
 
-for i in range(3):
-    t1.forward(100)
-    t1.left(90)
-for i in range(2):
-    t1.forward(80)
-    t1.left(90)
-for i in range(2):
-    t1.forward(60)
-    t1.left(90)
-t1.forward(40)
-t1.left(90)
-t1.forward(40)
+def rotate(i, angle):
+    if i < 2:
+        t[i].left(angle)
+    else:
+        t[i].right(angle)
 
-t2.right(180)
-for i in range(3):
-    t2.forward(100)
-    t2.right(90)
-for i in range(2):
-    t2.forward(80)
-    t2.right(90)
-for i in range(2):
-    t2.forward(60)
-    t2.right(90)
-t2.forward(40)
-t2.right(90)
-t2.forward(40)
 
-for i in range(3):
-    t3.forward(100)
-    t3.right(90)
-for i in range(2):
-    t3.forward(80)
-    t3.right(90)
-for i in range(2):
-    t3.forward(60)
-    t3.right(90)
-t3.forward(40)
-t3.right(90)
-t3.forward(40)
+t = []
+for i in range(4):
+    t.append(turtle.Turtle())
 
-t4.left(180)
-for i in range(3):
-    t4.forward(100)
-    t4.left(90)
-for i in range(2):
-    t4.forward(80)
-    t4.left(90)
-for i in range(2):
-    t4.forward(60)
-    t4.left(90)
-t4.forward(40)
-t4.left(90)
-t4.forward(40)
+for i in range(4):
+    if i % 2 != 0:
+        rotate(i, 180)
+    for j in range(3):
+        t[i].forward(100)
+        rotate(i, 90)
+    for j in range(2):
+        t[i].forward(80)
+        rotate(i, 90)
+    for j in range(2):
+        t[i].forward(60)
+        rotate(i, 90)
+    t[i].forward(40)
+    rotate(i, 90)
+    t[i].forward(40)
