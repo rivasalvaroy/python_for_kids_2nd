731f8ce07036f742edf54eb44aa7e787ebdf40bf
rivasalvaroy <rivasalvaroy@gmail.com>
2023-04-23 12:37:03 -0600

    ch10_04 - drawing a lot of rectangles

---
+++ examples/ch10/04_randomrect.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import random
+
+tk = Tk()
+canvas = Canvas(tk, height=400, width=400)
+canvas.pack()
+
+
+def random_rectangle(width, height):
+    x1 = random.randrange(width)
+    y1 = random.randrange(height)
+    x2 = x1 + random.randrange(width)
+    y2 = y1 + random.randrange(height)
+    canvas.create_rectangle(x1, y1, x2, y2)
+
+
+for x in range(0, 100):
+    random_rectangle(400, 400)
+canvas.mainloop()
